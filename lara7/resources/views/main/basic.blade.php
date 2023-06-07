42a9fe9e3ab7805edf70793757b91e7544104e1d
5llo <mhmdkhalil822@gmail.com>
2023-06-08 00:22:24 +0300

    family

---
+++ lara7/resources/views/main/basic.blade.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.rtl.min.css">
+
+
+
+<br>
+<br>
+
+
+<a href="/family" class="btn btn-primary" tabindex="-1" role="button" aria-disabled="true">all family</a>
+<a href="/family/create" class="btn btn-primary" tabindex="-1" role="button" aria-disabled="true">add family</a>
